eb4cb46e8352512b03fdf7aa46151fe3f4200935
Lucas <lucas@grepp.co>
2022-07-15 00:12:32 +0900

    reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,10 @@
   import Styling from "./lib/01. introduction/d_Styling.svelte";
   import NestedComponents from "./lib/01. introduction/e_NestedComponents.svelte";
   import HtmlTags from "./lib/01. introduction/f_HtmlTags.svelte";
+  import Assignments from "./lib/02. reactivity/a_Assignments.svelte";
+  import Declarations from "./lib/02. reactivity/b_Declarations.svelte";
+  import Statements from "./lib/02. reactivity/c_Statements.svelte";
+  import UpdatingArrayAndObject from "./lib/02. reactivity/d_UpdatingArrayAndObject.svelte";
 </script>
 
 <main>
@@ -15,6 +19,11 @@
   <Styling />
   <NestedComponents />
   <HtmlTags />
+  <!-- 02. reactivity -->
+  <Assignments />
+  <Declarations />
+  <Statements />
+  <UpdatingArrayAndObject />
 </main>
 
 <style>
